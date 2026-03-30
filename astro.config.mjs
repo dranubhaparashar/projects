ab2012960fe47179b4ceda3bd3c1d0f29f0f5a5a
dranubhaparashar <dranubhaparashar@gmail.com>
2026-03-30 23:40:28 +0530

    Update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://dranubhaparashar.github.io",
+  	base: "/projects",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
