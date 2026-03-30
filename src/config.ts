73a85551a7b45d0b99039c38075de5b76c40d08f
dranubhaparashar <dranubhaparashar@gmail.com>
2026-03-31 03:34:03 +0530

    Enable site banner

--- src/config.ts
+++ src/config.ts
@@ -45,8 +45,8 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			name: "Website",
+			url: "https://anubhaparashar.github.io/", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
