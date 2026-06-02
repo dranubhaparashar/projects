420bb80c1ca45d4c8293c422ee019671a3f7c947
Anubha Parashar <anubhaparashar1025@gmail.com>
2026-06-03 02:02:32 +0530

    Add new post

--- src/utils/post-id.ts
+++ src/utils/post-id.ts
@@ -1,15 +1,19 @@
-export function getProjectPostId(slugOrPath: string): string {
-	const clean = slugOrPath
+export function normalizePostId(pathname: string): string {
+	const clean = pathname
 		.split("?")[0]
 		.replace(/^\/+|\/+$/g, "")
 		.split("/")
 		.filter(Boolean)
 		.at(-1);
 
-	return (clean || slugOrPath)
+	return (clean || pathname)
 		.toLowerCase()
 		.normalize("NFKD")
 		.replace(/[\u0300-\u036f]/g, "")
 		.replace(/[^a-z0-9]+/g, "-")
 		.replace(/^-+|-+$/g, "");
+}
+
+export function getProjectPostId(slugOrPath: string): string {
+	return normalizePostId(slugOrPath);
 }
